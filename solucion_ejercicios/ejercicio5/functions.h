5ddc431b60376d4c5afc2c048bf0cc31da74ef71
avalero <alberto.valero@bq.com>
2020-04-04 12:37:44 +0200

    ejercicio 5

---
+++ solucion_ejercicios/ejercicio5/functions.h
@@ -0,0 +1,23 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+template <class T>
+T suma(T & a, T & b){
+    return a + b;
+}
+
+
+template <class T>
+T resta(T & a, T & b){
+    return a - b;
+}
+
+
+template <class T>
+T multiplicacion(T & a, T & b){
+    return a * b;
+}
+
+#endif // FUNCTIONS_H
+
+
